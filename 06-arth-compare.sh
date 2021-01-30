ec544c118ac27db3a7ecde40eabec909fe21d935
rnella01 <raghava.nellaturu@gmail.com>
2021-01-30 10:24:53 -0600

    initial commit

---
+++ 06-arth-compare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# declare integers
+NUM1=2
+NUM2=2
+if [ $NUM1 -eq $NUM2 ]; then
+	echo "Both Values are equal"
+else 
+	echo "Values are NOT equal"
+fi 
---
+++ 08-file-exists.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+file="./01-variables.sh"
+if [ -e $file ]; then
+	echo "File exists"
+else 
+	echo "File does not exists"
+fi 
